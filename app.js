48bd66883c265d1dac3fcc6afd141dbd3a9802fc
Matheus Vilar <mhvm2007@gmail.com>
2025-08-14 00:46:48 -0300

    Desafio 2 JavaScript

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let nomeUser = prompt ('Digite o seu nome')
-let saldo = 2500.55
-
-alert(`Olá ${nomeUser}, o seu saldo é de R$${saldo}`)
+let nomeUser = prompt ('Digite o seu Nome');
+alert(`Olá ${nomeUser}, estou meio perdido no tempo..`)
+let diaSemana = prompt('Hoje é Sabado ou Domingo? (S/D)')
+if (diaSemana == "S"){
+    alert('Aah obrigado, bom saber que hoje é sábado.. Bom final de semana!')
+} else{
+    alert('Hmmmm okay, amanhã eu trabalho :( boa semana a nós')
+}
